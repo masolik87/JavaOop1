a192f5406ec2626aec139a7bbf6f6075cde6ee37
Дмитрий Б <dimon20.07@mail.ru>
2023-02-16 18:54:46 +0300

    .\src\

---
+++ src/main/java/Farmer.java
@@ -0,0 +1,16 @@
+public class Farmer extends Archer {
+
+    public Farmer(float hp, int maxHp, int damage, int att, int def, int shots, int maxShots, float dist, String name) {
+        super(hp, maxHp, damage, att, def, shots, maxShots, dist);
+        super.name = name;
+    }
+
+    public Farmer(String name) {
+        super(150, 150, 12, 5, 3, 22, 22, 3);
+        super.name = name;
+    }
+    @Override
+    public String getInfo() {
+        return "Я Фермер!";
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,11 @@ public class Main {
 
         Sniper man = new Sniper("Иван");
         XBowMan man1 = new XBowMan(1.0f, 1, 2, 3, 5, 5, 5,5, "");
+        Farmer man2 = new Farmer(1f,1, 1, 2, 2, 3, 3, 5, "");
 
         System.out.println(man.getInfo());
         System.out.println(man1.getInfo());
-
+        System.out.println(man2.getInfo());
     }
 
     private static String getName(){
